cbc5a89b0845737474868798853c7319f2a3b9ba
Tejaswasekar <Tejaswasekar2002@gmail.com>
2025-12-30 15:18:55 +0530

    chore: save edits (App, Header) and add Grocery component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
 import RestaurantCard from "./components/RestaurantCard";
-import About from "./components/About";
 import ContactUs from "./components/ContactUs";
 import Error from "./components/Error";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import RestaurantMenu from "./components/RestaurantMenu";
+import Shimmer from "./components/Shimmer";
 
+// implementing lazy loading for Grocery component (code splitting, Dynamic Bunlding, On demand loading, Dynamic Importing)
+const Grocery = lazy(() => import("./components/Grocery"));
+
+const About = lazy(() => import("./components/About"));
 
 let Applayout = () => {
   return (
@@ -24,10 +28,14 @@ const appRouter = createBrowserRouter([
     path: "/",
     element: <Applayout />,
     errorElement: <Error />,
-    children:[
+    children: [
       {
         path: "/about",
-        element: <About />,
+        element: (
+          <Suspense fallback={<Shimmer></Shimmer>}>
+            <About />
+          </Suspense>
+        ),
       },
       {
         path: "/contact",
@@ -44,10 +52,17 @@ const appRouter = createBrowserRouter([
       {
         path: "/restaurant/:id",
         element: <RestaurantMenu />,
-      }
+      },
+      {
+        path: "/grocery",
+        element: (
+          <Suspense fallback={<Shimmer></Shimmer>}>
+            <Grocery />
+          </Suspense>
+        ),
+      },
     ],
   },
-  
 ]);
 
 let root = ReactDOM.createRoot(document.getElementById("root"));
---
+++ src/components/Grocery.js
@@ -0,0 +1,9 @@
+const Grocery = () => {
+  return (
+    <div>
+      <h1>Here all types of groceries are available here!!!! </h1>
+    </div>
+  );
+};
+
+export default Grocery;
